ca1083a0996c9ba80e37d739276bfb921675f88e
Christopher Sherman <chris@shermandigital.com>
2021-08-21 11:39:51 -0400

    Enable running server with HTTPS

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-require('dotenv').config({ silent: true })
-const express = require('express')
-const middleWarez = require('./index.js')
-const port = process.env.PORT || 3000
+require('dotenv').config({ silent: true });
+const express = require('express');
+const middleWarez = require('./index.js');
+const https = require('https');
+const fs = require('fs');
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
 
-const app = express()
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = https.createServer({ key, cert }, app);
 
 // Initial page redirecting to Github
-app.get('/auth', middleWarez.auth)
+app.get('/auth', middleWarez.auth);
 
 // Callback service parsing the authorization token
 // and asking for the access token
-app.get('/callback', middleWarez.callback)
+app.get('/callback', middleWarez.callback);
 
-app.get('/success', middleWarez.success)
-app.get('/', middleWarez.index)
+app.get('/success', middleWarez.success);
+app.get('/', middleWarez.index);
 
-app.listen(port, () => {
-  console.log("gandalf is walkin' on port " + port)
-})
+server.listen(port, () => {
+  console.log("gandalf is walkin' on port " + port);
+});
